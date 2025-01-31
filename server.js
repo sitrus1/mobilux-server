1d1fc4b29067f77f5592ce90859829364637866b
Sitrus1 <sirojiddin2010all@gmail.com>
2025-01-31 08:55:40 +0500

    ready?

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Убедись, что у тебя есть db.json
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
