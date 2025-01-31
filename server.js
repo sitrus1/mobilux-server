292f70c2c69031351825a777f24dce82dbd14841
Sitrus1 <sirojiddin2010all@gmail.com>
2025-01-31 09:18:42 +0500

    ready?

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
